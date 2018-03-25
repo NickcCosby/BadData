d1447679080b43abaf4feb8630f16fc00ed860d5
Nick Cosby <nickccosby@gmail.com>
2018-03-25 15:54:16 -0700

    Completed initial server side code

---
+++ BadData/apps/puzzles/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index),
+	url(r'^puzzle/(?P<number>[0-9]+)/$', views.getPuzzle),
+	url(r'^puzzle/(?P<number>[0-9]+)/win/$', views.wonPuzzle)
+]
