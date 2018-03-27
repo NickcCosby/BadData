c84f285e61413631a9e806f5a468702822be67a2
Ian_Danner <norseman117@gmail.com>
2018-03-27 13:53:22 -0700

    created Puzzle module and added puzzles to database along with Admin User

--- BadData/apps/puzzles/models.py
+++ BadData/apps/puzzles/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from ..login.models import User
 
 # Create your models here.
+class Puzzle(models.Model):
+    name = models.CharField(max_length=255)
+    quality_rating = models.IntegerField()
+    difficulty = models.IntegerField()
+    creator = models.ForeignKey(User, related_name="puzzles_created")
+    completed_by = models.ManyToManyField(User, related_name="puzzles_completed")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "<Puzzle object: {}, {}, {}, {}>".format(self.name,self.quality_rating,self.difficulty,self.creator,self.completed_by)
--- BadData/apps/puzzles/views.py
+++ BadData/apps/puzzles/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from ..login.models import User
+from models import *
 
 def index(request):
 	if 'user_id' not in request.session:
 		return redirect('/')
+	if 'user-puzzles' not in request.session:
+		request.session['user-puzzles'] = 'id'
+	adminCreated = Puzzle.objects.filter(creator=User.objects.get(id=1))
+	userCreated = Puzzle.objects.exclude(creator=User.objects.get(id=1))
 	context = {
+		"adminPuzzles" : adminCreated.order_by('id'),
+		"userPuzzles" : userCreated.order_by(request.session['user-puzzles']),
 		"user" : User.objects.get(id=request.session['user_id']),
 	}
 	return render(request,'puzzles/index.html',context)
@@ -30,3 +37,15 @@ def wonPuzzle(request, number):
 
 def getXML(request, number):
 	return HttpResponse(open('apps/puzzles/static/puzzles/xml/'+number+'.xml').read(), content_type='text/xml')
+
+def orderByDate(request):
+	request.session['user-puzzles'] = 'created_at'
+	return redirect('/BadData')
+
+def orderByDifficulty(request):
+	request.session['user-puzzles'] = 'difficulty'
+	return redirect('/BadData')
+
+def orderByName(request):
+	request.session['user-puzzles'] = 'name'
+	return redirect('/BadData')
