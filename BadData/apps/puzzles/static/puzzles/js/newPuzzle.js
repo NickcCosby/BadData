1e791f8f11a598ff0a038f413988973a4d3092f6
Nick Cosby <nickccosby@gmail.com>
2018-03-29 08:41:15 -0700

    Added validation for new puzzle form

--- BadData/apps/puzzles/static/puzzles/js/newPuzzle.js
+++ BadData/apps/puzzles/static/puzzles/js/newPuzzle.js
@@ -3,6 +3,26 @@ $(document).ready(function()
     update();
     $(".update").click(update);
     $(".submit").click(update);
+    $('.xAxisanswer').keypress(function(e){
+        // allowed char: 1 , 2 , 3, 4, 5, N, O, A, B, C
+        let allow_char = [49,50,51,52,53, 54, 55, 56, 57, 48, 44];
+        if(allow_char.indexOf(e.which) !== -1 ){
+          return true;
+        }
+        else{
+          return false;
+        }
+      });
+    $('.yAxisanswer').keypress(function(e){
+        // allowed char: 1 , 2 , 3, 4, 5, N, O, A, B, C
+        var allow_char = [49,50,51,52,53, 54, 55, 56, 57, 48, 44];
+        if(allow_char.indexOf(e.which) !== -1 ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
 });
 
 function update()
@@ -44,10 +64,6 @@ function update()
     {
         var xanswer = $(".xAxisanswer").val().split(",");
         var yanswer = $(".yAxisanswer").val().split(",");
-        xanswer[0] = xanswer[0].substring(1, xanswer.length);
-        xanswer[xanswer.length-1] = xanswer[xanswer.length-1].substring(0, xanswer[xanswer.length-1].length-1);
-        yanswer[0] = yanswer[0].substring(1, yanswer.length);
-        yanswer[yanswer.length-1] = yanswer[yanswer.length-1].substring(0, yanswer[yanswer.length-1].length-1);
         for(var iii = 0; iii < xanswer.length; iii++)
         {
             xanswer[iii] = Number(xanswer[iii])
@@ -65,5 +81,126 @@ function update()
         }
         updateGraph(data, answer, relationships);
         answerGraph(answer);
+        
+    }
+    
+
+}
+function submitPuzzle()
+{
+    update();
+    //validate
+    var data = {};
+    var errors = [];
+    for(var iii = 0; iii < $(".columnCount").val() - 1; iii++)
+    {
+        data[$($(".columnDef th")[iii]).find("input").val()] = [];
+        for(var zzz = 0; zzz < $(".rowCount").val() - 1; zzz++)
+        {
+            data[$($(".columnDef th")[iii]).find("input").val()].push($($($(".stuff tr")[zzz]).find("td")[iii]).find("input").val())
+        }
+    }
+    var relationships = 
+    {
+        'x':$(".relationshipX").val(),
+        'y':$(".relationshipY").val()
+    }
+    var answer = 
+    {
+        'x':null,
+        'y':null
+    }
+    if($(".xAxisanswer").val() != "" && $(".yAxisanswer").val() != "")
+    {
+        var xanswer = $(".xAxisanswer").val().split(",");
+        var yanswer = $(".yAxisanswer").val().split(",");
+        answer['x'] = xanswer;
+        answer['y'] = yanswer;
+        for(var iii = 0; iii < xanswer.length; iii++)
+        {
+            answer['x'][iii] = Number(answer['x'][iii])
+            answer['y'][iii] = Number(answer['y'][iii])
+        }
+
+        
+        
+    }
+    
+
+    if($(".name").val() == "")
+        errors.push("Puzzle needs a name")
+    if($(".difficulty").val() == "")
+        errors.push("Puzzle needs a difficulty")
+    else if(Number($(".difficulty").val()) < 1 || Number($(".difficulty").val() > 11))
+        errors.push("Puzzle difficulty must be within 1-10");
+    if($(".columnCount").val() == 1)
+        errors.push("Puzzle needs columns")
+    if($(".rowCount").val() == 1)
+        errors.push("Puzzle needs default values")
+    if($(".xAxisanswer").val() == "")
+        errors.push("Puzzle needs an awnser for the x axis")
+    if($(".yAxisanswer").val() == "")
+        errors.push("Puzzle needs an awnser for the y axis")
+    if($(".relationshipX").val() == "")
+        errors.push("Puzzle needs a relationship for the x axis")
+    if($(".relationshipY").val() == "")
+        errors.push("Puzzle needs a relationship for the y axis")
+    if (!(validateJsep(jsep(relationships['x']), data, getProperties()) && validateJsep(jsep(relationships['y']), data, getProperties())))
+    {
+        errors.push("Cannot parse relationships (illegal statement or non-existant identifier)")
+    }
+    if(errors.length == 0)
+    {
+        var tempx = $(".xAxisanswer").val()
+        var tempy = $(".yAxisanswer").val()
+        $(".xAxisanswer").val("[" + tempx + ']')
+        $(".yAxisanswer").val("[" + tempy + ']')
+        $("form").submit();
+    }
+    else
+    {
+        $(".errors").html("")
+        for(var iii = 0; iii < errors.length; iii++)
+        {
+            $(".errors").append("<p>"+errors[iii]+"</p>")
+        }
+        return false;
+    }
+}
+function validateJsep(jsepObj, data, properties)
+{
+    try
+    {
+        if(jsepObj.type == "BinaryExpression")
+        {
+            return (validateJsep(jsepObj.right, data, properties) && validateJsep(jsepObj.left, data, properties));
+        }
+        else if (jsepObj.type == "Identifier")
+        {
+            if(jsepObj.name in data)
+                return true;
+            else
+                return false;
+        }
+        else if (jsepObj.type == "MemberExpression")
+        {
+            if(jsepObj.object.name in data)
+            {
+                if(jsepObj.property.name in properties)
+                    return true;
+                else
+                    return false;
+            }
+            else 
+                return false;
+        }
+        else if (jsepObj.type == "Literal")
+            return true;
+        else
+            return false;
+    }
+    catch(err)
+    {
+        return false;
     }
 }
